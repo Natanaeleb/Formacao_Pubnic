7bd7348082632a8cf68cc9be2bd5baf5ad7a4855
Natanaele <natanaelebalic@gmail.com>
2021-11-05 12:58:49 -0300

    Solucionado

--- Desafios/Desafio011.py
+++ Desafios/Desafio011.py
@@ -1,9 +1,10 @@
-linha1 = input().split() #separar as entradas
-cod1,num1,val1 = linha1 #converter para inteiro
+linha1 = input().split(" ") 
+linha2 = input().split(" ") 
 
-linha2 = input().split() #separar as entradas
+cod1,num1,val1 = linha1
 cod2,num2,val2 = linha2
 
-VP = (int(num1))*(float(val1)) + (int(num2))*(float(val2))
+VP = (int(num1) * float(val1)) + (int(num2) * float(val2))
 
-print('VALOR A PAGAR = R$ %.2f'%VP,end='\n')
+print("VALOR A PAGAR: R$ %.2f"%VP)
+
