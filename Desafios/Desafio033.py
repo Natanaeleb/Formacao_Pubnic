19a10eb090bbc6ae2d20bd7a7aace9f886430012
Natanaele <natanaelebalic@gmail.com>
2021-11-12 14:22:56 -0300

    Desafio 33 e 34

---
+++ Desafios/Desafio033.py
@@ -0,0 +1,6 @@
+N = int(input())
+i = 1
+
+for i in range(10):
+    mult = (i+1)* N
+    print(f'{i+1} x {N} = {mult}')
---
+++ Desafios/Desafio034.py
@@ -0,0 +1,10 @@
+
+while True:
+    validar = int(input())
+    if validar == 2002:
+        print('Acesso Permitido')
+        break
+    else:
+        print('Senha Invalida')
+        
+
